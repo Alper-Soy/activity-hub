1453889630514b0e8105ca3fe0680eb94398008d
soyalper <soy.m.alper@gmail.com>
2024-08-20 12:11:07 +0300

    AH-12 server side identity

---
+++ API/Contracts/Auth/AuthUserDto.cs
@@ -0,0 +1,9 @@
+namespace API.Contracts.User;
+
+public class AuthUserDto
+{
+    public string DisplayName { get; set; }
+    public string Token { get; set; }
+    public string Image { get; set; }
+    public string Username { get; set; }
+}
