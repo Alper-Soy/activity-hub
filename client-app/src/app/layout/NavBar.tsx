837943af0c3d916d62d5ff7a360a4ec95a6704d2
soyalper <soy.m.alper@gmail.com>
2024-08-27 02:47:00 +0300

    AH-22 pre continuous integration

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -15,7 +15,7 @@ export default observer(function NavBar() {
           Activity Hub
         </Menu.Item>
         <Menu.Item as={NavLink} to="/activities" name="Activities" />
-        <Menu.Item as={NavLink} to="/errors" name="Errors" />
+        {/* <Menu.Item as={NavLink} to="/errors" name="Errors" /> */}
         <Menu.Item>
           <Button
             as={NavLink}
